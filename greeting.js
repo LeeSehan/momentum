40b1319bbefa8bcab092bddda2f53a00617793fd
LeeSehan <dltpgks711@naver.com>
2020-05-08 22:50:15 +0900

    saving the user name

--- greeting.js
+++ greeting.js
@@ -5,8 +5,15 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
-function handleSubmit(event){
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
 
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
 }
 
 function askForName(){
